4fcd9aa21d24ead453d3d8227d326add1b0434e5
SiddhikaBhagat <165668920+SiddhikaBhagat@users.noreply.github.com>
2025-12-12 17:25:02 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all js code as newer version
+//alert(3 + 3) // we are using node js not browser
+let name="siddhika";
+let age=20;
+
+//primitive datatypes
+// number -- 2 to power 53
+// bigint
+// String -""
+// boolean -- true/false
+// null -- standalone value
+// undefined --
+// symbol -- most use in react (uniqueness of components)
+
+//object
+
+console.log(typeof "Syhhvbvb");
+console.log(typeof age);
+console.log(typeof(name))
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
