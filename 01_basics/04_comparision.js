7e769dd62869c0d94d8819b334480cac7d2ac7bb
SiddhikaBhagat <165668920+SiddhikaBhagat@users.noreply.github.com>
2025-12-12 20:56:42 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,24 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+ //console.log("2" > 1);
+ //console.log("02" > 1); 
+ // >,>=,<=, < convert string to number
+
+
+ //>,>=,<=, < convert string to number // == not convert
+ console.log(null > 0);//null to zero
+ console.log(null == 0);
+ console.log(null >= 0);
+
+console.log(undefined == 0); //always gives false 
+ console.log(undefined > 0);
+ console.log(undefined < 0);
+
+// === 
+
+ console.log("2" === 2);
