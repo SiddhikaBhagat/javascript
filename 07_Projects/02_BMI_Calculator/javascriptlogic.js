8e638a1e8bd3d8d0c425089f7ce62e1b8eded4f1
SiddhikaBhagat <165668920+SiddhikaBhagat@users.noreply.github.com>
2026-01-03 18:10:33 +0000

    modify bmi

--- 07_Projects/02_BMI_Calculator/javascriptlogic.js
+++ 07_Projects/02_BMI_Calculator/javascriptlogic.js
@@ -16,6 +16,13 @@ form.addEventListener('submit', function (e) {
   } else {
     const bmi = (weight / ((height * height) / 10000)).toFixed(2);
     //show the result
-    results.innerHTML = `<span>${bmi}</span>`;
+    if(bmi < 18.6){
+    results.innerHTML = `<span>${bmi} UnderWeight </span>`;
+}
+ else if(bmi >= 18.6 && bmi <= 24.9){
+    results.innerHTML = `<span>${bmi} NormalRange</span>`;
+}else{
+    results.innerHTML = `<span>${bmi} Overweight</span>`;
+}
   }
 });
